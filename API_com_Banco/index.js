d020106324b3c0dc59bd6216f4aee96140949606
Guilherme <guilhermemichalach@gmail.com>
2021-08-30 11:53:34 -0300

    bd

---
+++ API_com_Banco/index.js
@@ -0,0 +1,47 @@
+const { request, response, json } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Importando as rotas
+
+const usersRoutes = require("./routes/usersRoutes");
+
+// Definindo os middlewares
+ 
+app.use(express.json());
+
+// Definindo as rotas
+
+app.use("/users", usersRoutes);
+ 
+// Rotas da raíz
+app.get("/", (request, response) => {
+    response.send("Olá, Mundo!");
+});
+ 
+app.post("/", (request, response) => {
+    response.send("Método POST");
+});
+ 
+app.put("/", (request, response) => {
+    response.send("Método PUT");
+});
+ 
+app.delete("/", (request, response) => {
+    response.send("Método DELETE");
+});
+ 
+// Banco de dados
+ 
+// Rotas de usuário "/users"
+ 
+// Obter um usuário em específico
+ 
+// Criar um usuário
+
+// Atualizar as informções de um usuário
+
+// Deletar um usuário
+
+app.listen(PORT, () => console.log("O servidor está rodando..."));
